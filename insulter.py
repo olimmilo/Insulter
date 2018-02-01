9d915e34adb3ca3954670a1ba0df623d9277c1b7
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2018-02-01 16:02:46 -0500

    Updated from Brython Server: 2/1/2018 4:02:45 PM

--- insulter.py
+++ insulter.py
@@ -1,5 +1,11 @@
 name = input("Say your name you idiot, with more than 3 letters or else")
 
+name = name.lower
+nlistp = list(name)
+nlist = [nlistp[0],nlistp[1], nlistp[2]]
+
+print = nlist
+
 num = randint(1,1383)
 
 List = open("list.txt").readlines()
