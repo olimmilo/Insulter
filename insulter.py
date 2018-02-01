76e3efa511d06b25d30bee0c2eb1bbbe465071e2
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2018-02-01 17:20:29 -0500

    Updated from Brython Server: 2/1/2018 5:20:28 PM

--- insulter.py
+++ insulter.py
@@ -14,7 +14,7 @@ print((alphabet[nlistp[1]])%13820)
 i = 0
 while i <= len(nlistp):
     let=nlistp[i]
-    #nlist.append((alphabet[let]))
+    nlist.append((alphabet[let]))
     print(let)
     i=i+1
 
